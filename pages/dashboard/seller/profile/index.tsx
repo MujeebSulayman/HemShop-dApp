a903ee4b9ae22e66bf47faf56a8c84c37d64fca4
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-11-02 10:04:40 +0100

    remove alloy

--- pages/dashboard/seller/profile/index.tsx
+++ pages/dashboard/seller/profile/index.tsx
@@ -1,14 +1,7 @@
-import DashboardLayout from '@/components/dashboard/DashboardLayout'
 import React from 'react'
 
-const SellerProfilePage = () => {
-  return (
-    <DashboardLayout userRole="seller">
-      <div className="space-y-6">
-        <h1 className="text-2xl font-bold">Seller Profile</h1>
-      </div>
-    </DashboardLayout>
-  )
+const index = () => {
+  return <div>Seller Profile</div>
 }
 
-export default SellerProfilePage 
+export default index

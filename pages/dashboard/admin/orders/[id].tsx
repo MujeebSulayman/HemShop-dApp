c1a8e473966a39a6422fa8d842c8e5be503447c2
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-11-07 12:05:54 +0100

    Order Update

---
+++ pages/dashboard/admin/orders/[id].tsx
@@ -0,0 +1,12 @@
+import withAdminLayout from '@/components/hoc/withAdminLayout'
+import React from 'react'
+
+const OrderDetail = () => {
+  return (
+    <div>
+      <h1>Order Detail</h1>
+    </div>
+  )
+}
+
+export default withAdminLayout(OrderDetail)
